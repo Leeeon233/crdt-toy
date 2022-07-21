4a34e90ad7d69eaa3d5c6125df48b7d64179d9b4
leeeon233 <leeeon233@gmail.com>
2022-07-21 21:05:02 +0800

    feat: add sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { sumOfSquares, init } from './wasmEntry';
-import { Box, Center,Text,Textarea, Square, Circle, Container, Stack, HStack, VStack  } from '@chakra-ui/react';
+import { Box, Center, Text, Textarea, Container, HStack, VStack  } from '@chakra-ui/react';
 import Client from './client';
 
 function App() {
@@ -12,14 +12,18 @@ function App() {
     })();
   }, []);
 
+  const onSync = (clientId: number|string)=>{
+    return "hhh"
+  };
+
   return (
     <div className="App">
       <Container w='100%' h="100vh">
         <Center>
           <VStack spacing='50'>
             <HStack spacing='200' pt='100'>
-              <Client clientId={1}/>
-              <Client clientId={2}/>
+              <Client clientId={1} onSync={onSync}/>
+              <Client clientId={2} onSync={onSync}/>
             </HStack>
             <Box w='100%'>
               <Center>
--- src/client.tsx
+++ src/client.tsx
@@ -1,17 +1,34 @@
-import { useEffect, useState } from 'react';
-import { Box, Center,Button, Textarea, Text, Circle,Flex , Container, Stack, HStack, VStack  } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Button, Textarea, Text, Flex } from '@chakra-ui/react';
 
 interface IClientProps {
   clientId: number|string;
+  onSync: (clientId: number|string)=>string;
 };
 
-const Client = ({clientId}:IClientProps)=> {
+const Client = ({clientId, onSync}:IClientProps)=> {
+  const [isSync, setIsSync] = useState(false);
+  const [text, setText] = useState('');
+
+  const onClick = ()=>{
+    setIsSync(true);
+    setText(onSync(clientId)); // TODO: call wasm function
+    setIsSync(false);
+  }
+
+  const onChange = (e:any)=> {
+    if(!isSync) {
+      const t = e.target.value;
+      setText(t)
+      // TODO call wasm function
+    }
+  }
 
   return (
       <Flex w='300px' h="300px" direction='column'>
         <Text m='8px'> Client: {clientId}</Text>
-        <Textarea flex='1' placeholder='Here is a sample placeholder' resize='none'/>
-        <Button>获取同步</Button>
+        <Textarea value={text} onChange={onChange} flex='1' placeholder='Here is a sample placeholder' resize='none'/>
+        <Button onClick={onClick}>获取同步</Button>
       </Flex>
   );
 }
